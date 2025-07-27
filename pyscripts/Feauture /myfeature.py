63f0362d9cf5392f2d8e568d2d564c5ddc6db1d8
Richard <infobravoconfig99@gmail.com>
2025-07-27 14:00:14 +1000

    create feature folder

---
+++ pyscripts/Feauture /myfeature.py
@@ -0,0 +1,9 @@
+
+def greet(name):
+    """Return a greeting message for the given name."""
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    print(greet(user_name))
